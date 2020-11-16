535a4f3c22689f6af853ee36125c1ff422757f38
DigitalDsntHurt <digitaldoesnthurt@gmail.com>
2020-11-16 12:16:36 -0800

    First commit, function outlined.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+var getElementsByClassName = function(className) {
+  var matchElements = [];
+
+  var helper = function(element) {
+    if (element.classList.includes(className)) {
+      matchElements.push(element);
+    }
+
+    var children = element.childNodes;
+    // for (var child of children) {
+    //   helper(child);
+    // }
+    for (var i = 0; i < children.length; i++) {
+      helper(children[i]);
+    }
+  };
+
+  helper(document.body);
+  return matchElements;
 };
+
+// I : string class name
+// O : array of dom nodes
